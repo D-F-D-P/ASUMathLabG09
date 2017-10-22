acf7855cefcf715bd6739ac4877726fa320dc455
youmi20 <32936790+youmi20@users.noreply.github.com>
2017-10-22 20:09:46 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,14 @@
 //Write your function here
+int** subtract_matrix(int** A, int** B, int rows, int columns)
+{
+	int** result = createEmptyMatrix(rows, columns);
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < columns; j++)
+		{
+			result[i][j] = A[i][j] - B[i][j];
+		}
+	}
+
+	return result;
+}
