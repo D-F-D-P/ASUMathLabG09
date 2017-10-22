053369b970539c393acd2444bdf4ed54dfd79417
WaleedEmadEzat <32915573+WaleedEmadEzat@users.noreply.github.com>
2017-10-22 20:19:20 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,9 @@
-//Write your function here
+void destroy_matrix(int** matrix, int rows)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		delete[] matrix[i]; //deletes an inner array of integer;
+	}
+  
+	delete[] matrix; //delete pointer holding array of pointers;
+}
