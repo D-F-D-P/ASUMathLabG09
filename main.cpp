834248a353dea1aa9546677f00449366ca20bb66
youmnamahmoud <30958855+youmnamahmoud@users.noreply.github.com>
2017-10-22 09:23:30 +0300

    print matrix

--- main.cpp
+++ main.cpp
@@ -1,1 +1,10 @@
-//Write your function here
+//print matrix
+
+void print_matrix(int**A,int rows,int columns){
+	for(int i=0;i<rows;i++){
+		for(int j=0;j<columns;j++){
+			cout<<A[i][j]<<"\t";
+		}
+		cout<<endl;
+	}
+}
