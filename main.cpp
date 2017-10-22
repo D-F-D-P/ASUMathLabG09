3bbdef63ae99a154b8eca7f49b79b9ed942603d8
Nader Yasser <nader.yasser.93@gmail.com>
2017-10-22 16:45:39 +0200

    NADER

--- main.cpp
+++ main.cpp
@@ -1,1 +1,14 @@
-//Write your function here
+int** sum_matrix(int** A, int** B, int rows, int columns)
+{
+	int** result = createEmptyMatrix(rows, columns);
+
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < columns; j++)
+		{
+			result[i][j] = A[i][j] + B[i][j];
+		}
+	}
+
+	return result;
+}
