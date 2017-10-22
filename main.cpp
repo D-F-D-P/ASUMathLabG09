c4765569c33a3c5782c0e8aaef05ca421e731ae1
YomnaJehad <32596134+YomnaJehad@users.noreply.github.com>
2017-10-22 15:30:48 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,15 @@
-//Write your function here
+//Multiply
+int** multiply_matrix(int** A, int** B, int rows, int columns)
+{
+	int** result = createEmptyMatrix(rows, columns);
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < columns; j++)
+		{
+			for (int k = 0; k < columns; k++)
+			result[i][j] += A[k][j] * B[i][k];
+		}
+	}
+
+	return result;
+}
