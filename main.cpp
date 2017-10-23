da90cafc3b4ecc1b348feabcbb97493b306e7a50
youssephAhmed <yousseffahmed@hotmail.com>
2017-10-23 02:08:32 +0200

    fill matrix

---
+++ main.cpp
@@ -0,0 +1,15 @@
+int** fill_matrix(int** A, int rows, int columns)
+{
+	cout << "Please fill the matrix with its elements" << endl;
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < columns; j++)
+		{
+			cin >> A[i][j];
+			cout << "\t";
+		}
+		cout << endl;
+	}
+
+	return A;
+}
