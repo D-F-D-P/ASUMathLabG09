a648c7b3ff166c50ffaaa9deec0cec0c9f817820
youmnamahmoud <30958855+youmnamahmoud@users.noreply.github.com>
2017-10-24 21:30:35 +0200

    print matrix updated "double"

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 //print matrix
 
-void print_matrix(int**A,int rows,int columns){
+void print_matrix(double** A,int rows,int columns){
 	for(int i=0;i<rows;i++){
 		for(int j=0;j<columns;j++){
 			cout<<A[i][j]<<"\t";
