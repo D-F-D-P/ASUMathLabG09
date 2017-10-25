e694fd72459850b9f9a088b7e0f6fa00b9724e01
youssephAhmed <yousseffahmed@hotmail.com>
2017-10-25 18:52:32 +0200

    fill matrix with float

--- main.cpp
+++ main.cpp
@@ -1,5 +1,21 @@
 #include "matrix.h"
 
+double** fill_matrix(double** A, int rows, int columns)
+{
+	cout << "Please fill the matrix with its elements" << endl;
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < columns; j++)
+		{
+			cin >> A[i][j];
+			cout << "\t";
+		}
+		cout << endl;
+	}
+
+	return A;
+}
+
 int main(){
 	return 0;
 }
