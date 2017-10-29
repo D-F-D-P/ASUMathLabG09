94fd312c7d4d691d14d826b1fd0ad6e66975a3bf
YomnaJehad <32596134+YomnaJehad@users.noreply.github.com>
2017-10-29 13:57:17 +0200

    Update matrix.cpp

--- matrix.cpp
+++ matrix.cpp
@@ -210,3 +210,29 @@ void matrix::print_matrix()
     return newMatrix;
 }*/
 
+
+matrix matrix::operator * (matrix& m){
+    if (this->columns!=m.rows)
+      //Or whatever the doctor says
+	    cout << "error columns of first is not equal rows of the second" << endl;
+    matrix r(this->rows,m.columns);
+
+    //initializing elements of matrix r=0
+    for (int i=0;i<this->rows;i++){
+        for (int j=0;j<m.columns;j++){
+            r.elements[rows][columns]=0;
+        }
+    }
+
+    // Multiplying and store in r
+     for (int i=0;i<this->rows;i++){
+        for (int j=0;j<m.columns;j++){
+            for (int k=0;k<this->columns;k++){
+                r.elements[rows][columns] += this->elements[rows][k]*m.elements[k][columns];
+            }
+        }
+    }
+    return r;
+}
+
+
