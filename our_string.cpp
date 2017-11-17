5423bee0b741291c99530af92bdde5829855dae1
younanNagy <younan.nagy2@gmail.com>
2017-11-17 17:40:14 +0200

    decode branch 1st commit

--- our_string.cpp
+++ our_string.cpp
@@ -10,11 +10,11 @@ bool our_string :: Is_it_num(our_string input, double &num)
 {
 
 }
-int our_string:: find_str(our_string a, int &how_many)
+int our_string:: find_str(our_string a, int &how_many, int start)
 {
 
 }
-int our_string :: find_2str(our_string a, our_string b)
+int our_string :: find_2str(our_string a, our_string b, int start)
 {
 
 }
--- our_string.h
+++ our_string.h
@@ -7,5 +7,6 @@ public:
 	our_string Trim(char a);
 	bool Is_it_num(our_string input, double &num);
 	int find_str(our_string a, int &how_many,int start);
+
 	int find_2str(our_string a, our_string b,int start);
 };
