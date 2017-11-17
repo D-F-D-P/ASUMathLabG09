1eeef26951406ad6895f2233bd8023c007bf86b0
younanNagy <younan.nagy2@gmail.com>
2017-11-17 17:04:04 +0200

    our string class modifications -int start (only)-

---
+++ our_string.cpp
@@ -0,0 +1,20 @@
+#include "our_string.h"
+using namespace std;
+
+
+our_string our_string:: Trim(char a)
+{
+	
+}
+bool our_string :: Is_it_num(our_string input, double &num)
+{
+
+}
+int our_string:: find_str(our_string a, int &how_many)
+{
+
+}
+int our_string :: find_2str(our_string a, our_string b)
+{
+
+}
