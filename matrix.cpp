de348153545c320f55a2307fe81d373e1b489c11
youssephAhmed <yousseffahmed@hotmail.com>
2018-01-11 15:40:23 +0200

    fixing a memory leakage bug

--- matrix.cpp
+++ matrix.cpp
@@ -46,9 +46,7 @@ matrix::matrix(int rows, int columns)
         elements[i] = new double[columns];
     }
 
-
     empty_matrix();
-
 
 }
 
@@ -300,9 +298,7 @@ void matrix::reset_matrix(int rows, int columns)
         this -> elements[i] = new double[columns];
     }
 
-
     empty_matrix();
-
 
 }
 
@@ -733,12 +729,10 @@ matrix matrix :: operator / (matrix &m)
     if(is_equal((*this) , m))
     result.unity_matrix();
 
-
    // else if (m.get_determinant() == 0 || isnan(m.get_determinant()))
     //{
       //  cout << "Error determinant = 0" <<endl;
     //}
-
 
     else
     divide_matrix((*this), m , result);
@@ -812,6 +806,8 @@ double matrix::get_determinant()
     {
         result *= temp -> elements[i][i];
     }
+
+    delete temp; // $$$$$$
     return result;
 
 
@@ -826,42 +822,6 @@ double matrix::get_determinant()
         m *= -1;
     }
     return value;*/
-
-}
-
-//Function for filling the matrix with zeros
-
-void matrix:: zeros(){
-
-	for(int i=0; i<rows; i++){
-		for(int j=0; j< columns; j++){
-			elements[i][j]=0;
-        }
-	}
-
-}
-//Function for filling the matrix with random no.
-
-void matrix:: rand(){
-
-	for(int i=0; i<rows; i++){
-		for(int j=0; j< columns; j++){
-			elements[i][j]=(rand()%10) / 10.0;
-        }
-	}
-
-}
-
-
-//Function for filling the matrix with ones
-
-void matrix:: ones(){
-
-	for(int i=0; i<rows; i++){
-		for(int j=0; j< columns; j++){
-			elements[i][j]=1;
-        }
-	}
 
 }
 
