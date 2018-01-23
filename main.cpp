986abd75c55db94ae6e5a1066395241999eb6d6f
youmi20 <32936790+youmi20@users.noreply.github.com>
2018-01-23 18:50:23 +0200

    error handling from 0 to 9

--- main.cpp
+++ main.cpp
@@ -1,13 +1,43 @@
-double** subtract_matrix(int** A, int** B, int rows, int columns)
-{
-	double** result = createEmptyMatrix(rows, columns);
-	for (int i = 0; i < rows; i++)
-	{
-		for (int j = 0; j < columns; j++)
-		{
-			result[i][j] = A[i][j] - B[i][j];
-		}
-	}
+#include <iostream>
+#include <string>
+#include <fstream>
+#include "math_lab.h"
+#include "matrix.h"
+#include "dynamic_array_matrix.h"
+#define MATRIX array_matrix.get_matrix()
 
-	return result;
+
+using namespace std;
+
+
+int main(int argc, char* argv[])
+{
+try{
+ math_lab our_math_lab;
+
+ if(argc>1)
+  our_math_lab.load_file(argv[1]);
+
+ else
+  our_math_lab.open_command();
+  
 }
+catch(int error){
+
+  if(error==0) cout<<"error input: rows or columns is a negative value, program terminated."<<endl;
+  if(error==1) cout<<"error input: matrix element is not a valid input, expected double."<<endl;
+  if(error==2) cout<<"error input: fill matrix error, conflict in columns number."<<endl;
+  if(error==3) cout<<"error: calling matrix has 0 rows or 0 columns ,sub matrix can't be created."<<endl;
+  if(error==4) cout<<"error input: required sub matrix is larger than calling matrix"<<endl;
+  if(error==5) cout<<"error:not square matrix, determinant can't be evaluated."<<endl;
+  if(error==6) cout<<"error:not square matrix ,inverse can't be evaluated."<<endl;
+  if(error==7) cout<<"error:zero denominator"<<endl;
+  if(error==8) cout<<"error input: not square matrix unity matrix can't be created."<<endl;
+  if(error==9) cout<<"error input: rows or columns =0 unity matrix can't be created."<<endl;
+}
+
+	return 0;
+
+}
+
+
